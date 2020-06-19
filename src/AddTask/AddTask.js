76dcfc85185ce1b9a6925c0848c8f408f6c5f739
BonIlcer <pantaev.e@mail.ru>
2020-06-20 06:39:14 +0700

    Add nav bar

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -6,13 +6,22 @@ function AddTask({ dispatch }) {
   const updateInput = (e) => {
     setDescription(e.target.value);
   };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") addTask();
+  };
   function addTask() {
     dispatch({ type: "add", description: description });
     setDescription("");
   }
+
   return (
     <div className="add-task-wrapper">
-      <input type="text" value={description} onChange={updateInput} />
+      <input
+        type="text"
+        value={description}
+        onChange={updateInput}
+        onKeyDown={handleKeyDown}
+      />
       <button onClick={addTask}>Submit</button>
     </div>
   );
--- src/App.js
+++ src/App.js
@@ -2,13 +2,40 @@ import React from "react";
 import "./App.css";
 import "./ToDoList/ToDoList";
 import ToDoList from "./ToDoList/ToDoList";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ToDoList />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <ToDoList />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
+}
+
+function About() {
+  return <h2>About project</h2>;
 }
 
 export default App;
