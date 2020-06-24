d9c76e860e56f47d6a293919d3020f961216ab58
BonIlcer <pantaev.e@mail.ru>
2020-06-24 14:14:24 +0700

    Add logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./ToDoList/ToDoList";
 import ToDoList from "./ToDoList/ToDoList";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.jpg";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
+        <div className="nav-bar">
+          <img className="logo" src={logo} alt="logo"></img>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -17,18 +19,16 @@ function App() {
               <Link to="/about">About</Link>
             </li>
           </ul>
-
-          <hr />
-
-          <Switch>
-            <Route exact path="/">
-              <ToDoList />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
         </div>
+
+        <Switch>
+          <Route exact path="/">
+            <ToDoList />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
